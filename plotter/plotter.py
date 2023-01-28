bbfc3923261162ea4aaa1c48cdc1b4d68363adb3
catubc <mitelutco@gmail.com>
2023-01-28 14:42:07 +0100

    ylims changed

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -538,7 +538,7 @@ class PlotROIs():
                                                   interpolation="none")
             #
             self.ax_image.set_xlim(0,512)
-            self.ax_image.set_ylim(0,512)
+            self.ax_image.set_ylim(512,0)
 
             ################################################
             ############## PLOT ROI CONTOURS ###############
